46fcd7cb2e685f5abc046a91d9db37322d6d983a
Edilbert Loquine <edilbert.loquine@coreproc.ph>
2019-03-25 09:52:32 +0800

    Update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,70 @@
-@extends('layouts.app')
+<!doctype html>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<html lang="en">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+<head>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Class Monitor') }}</title>
+
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+    <link href="{{ asset('assets/css/material-kit.min.css') }}" rel="stylesheet"/>
+
+    <style>
+        .page-bg {
+            background-image: url({{ asset('assets/img/university.jpg') }});
+            background-size: cover;
+            background-position: top center;
+        }
+
+        .footer-bottom {
+            color: white;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            right: 0;
+        }
+    </style>
+</head>
+
+<body>
+<div class="page-header header-filter purple-filter page-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="card card-login">
+                    <form class="form" method="POST" action="{{ route('login') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
+                        <div class="card-header card-header-warning text-center">
+                            <h4 class="card-title">Login</h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                      <i class="material-icons">mail</i>
+                                    </span>
+                                </div>
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email" required autofocus>
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text">
+                                      <i class="material-icons">lock_outline</i>
+                                    </span>
+                                </div>
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
@@ -37,37 +72,54 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
+                            <div class="form-check" style="padding: 0; padding-left: 2px;">
+                                <label class="form-check-label">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                                    Remember me?
+                                    <span class="form-check-sign">
+                                        <span class="check"></span>
+                                    </span>
+                                </label>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="footer text-center">
+                            <button type="submit" class="btn btn-warning">Sign In</button>
+{{--                            <a class="btn btn-warning" href="{{ route('register') }}">Sign Up</a>--}}
+                        </div>
+                        <div class="footer text-center">
+                            <a href="{{ route('password.request') }}">Forgot password?</a>
                         </div>
                     </form>
                 </div>
             </div>
+
+            <div class="col-md-6">
+                <h1>LOGO HERE</h1>
+            </div>
         </div>
     </div>
+    <footer class="footer-bottom">
+        <div class="container">
+            <div class="copyright">
+                &copy;
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> Class Monitor
+            </div>
+        </div>
+    </footer>
 </div>
-@endsection
+
+<script src="{{ asset('assets/js/core/jquery.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/core/popper.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/core/bootstrap-material-design.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/plugins/moment.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/plugins/bootstrap-datetimepicker.js') }}" type="text/javascript"></script>
+<script src="{{ asset('assets/js/plugins/nouislider.min.js') }}" type="text/javascript"></script>
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<script src="{{ asset('assets/js/material-kit.js?v=2.0.5') }}" type="text/javascript"></script>
+
+</body>
+
+</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -68,6 +68,11 @@
                                 </div>
                             </li>
                         @endguest
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
                     </ul>
                 </div>
             </div>
